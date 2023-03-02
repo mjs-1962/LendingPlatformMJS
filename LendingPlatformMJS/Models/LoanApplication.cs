b435a755b2835adbb29899795b16bb448ccbfac9
mjs-1962 <mark.swainson@btinternet.com>
2023-03-02 14:56:50 +0000

    Add project files.

---
+++ LendingPlatformMJS/Models/LoanApplication.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LendingPlatformMJS.Models
+{
+    internal class LoanApplication
+    {
+        public string Id = Guid.NewGuid().ToString();
+        public decimal LoanAmount { get; set; }
+        public decimal AssetValue { get; set; }
+        public int CreditScore { get; set; }
+
+        public bool LoanApproved { get; set; }
+
+        public string RejectionReason { get; set; } = string.Empty;
+
+        public Decimal LTV()
+
+        {
+            if ((LoanAmount >= 0 ) && (AssetValue >= 0 )) 
+            { 
+            return (LoanAmount / AssetValue) * 100;
+            }
+            return 0;
+
+        }
+            }
+}
---
+++ LendingPlatformMJS/Program.cs
@@ -0,0 +1,150 @@
+﻿using LendingPlatformMJS.Models;
+using System;
+
+namespace LendingPlatform
+{ 
+    class Program
+    {
+
+        public static List<LoanApplication>? loanApplications { get; set; }
+
+        public static bool finished = false;
+
+        public static void Main(string[] args)
+        {
+            loanApplications = new List<LoanApplication>();
+
+            do
+            {
+                MainMenu();
+                int option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        ApplyForLoan();
+                        break;
+                    case 2:
+                        ShowLoanApplicationResultsSummary();
+                        break;
+                    case 3:
+                        Console.WriteLine("You Chose 3");
+                        break;
+                    case 4:
+                        Console.WriteLine("You Chose 4");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Seelction");
+                        break;
+                }
+            } while (!finished);
+            //Console.ReadLine();
+        }
+
+
+        static void MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Please Enter A Number from 1 To 4 To Choose An Option From The Menu Below");
+            Console.WriteLine("1 - Enter Loan Details");
+            Console.WriteLine("2 - Total Applicants To date");
+            Console.WriteLine("3 - Total Value Of Loans Written To date");
+            Console.WriteLine("4 - Mean Average Of Loan To Value Of Applications To Date");
+        }
+
+
+        static void ShowLoanApplicationResultsSummary()
+        {
+            int totalApplications = loanApplications.Count();
+            int succesfullLoans = loanApplications.Count(p => p.LoanApproved);
+            int rejectedLoans = loanApplications.Count(p => p.LoanApproved == false);
+            //Decimal TotalLoansWritten = loanApplications.Sum(p => p.LoanAmount where p.LoanApproved == true);
+
+            var TotalLoansWritten =
+            from la in loanApplications
+            group la by la.LoanApproved into loanGroup
+            select new
+            {
+                Team = loanGroup.Key,
+                LoanTotal = loanGroup.Sum(x => x.LoanAmount),
+            };
+
+
+            Console.Clear();
+            Console.WriteLine($"Application Summary Total: {totalApplications} Successful {succesfullLoans} Rejected {rejectedLoans}");
+            Console.ReadLine();
+        }
+
+        static void ApplyForLoan()
+        {
+            Console.Clear();
+            Console.WriteLine("Please Details Below Below");
+            Console.WriteLine(" ");
+            Console.WriteLine("Enter The Loan Amount");
+            string? loanAmount = Console.ReadLine();
+            Console.WriteLine("Enter The Asset Value");
+            string? assetValue = Console.ReadLine();
+            Console.WriteLine("Enter The Credit Score");
+            string? creditScore = Console.ReadLine();
+
+            decimal _loanAmount = 0;
+            decimal _assetValue = 0;
+            int _creditScore = 0;
+
+           
+
+            Decimal.TryParse(loanAmount, out _loanAmount);
+            Decimal.TryParse(assetValue, out _assetValue);
+            int.TryParse(creditScore, out _creditScore);
+
+            LoanApplication loanApplication = new LoanApplication()
+            {
+                LoanAmount =_loanAmount,
+                AssetValue = _assetValue,
+                CreditScore = _creditScore
+            };
+
+            ValidateLoan(loanApplication);
+
+            loanApplications.Add(loanApplication);
+
+            if (loanApplication.LoanApproved)
+            {
+                Console.WriteLine($"Loan Application No. {loanApplication.Id} Was Successful");
+            }
+            else
+            {
+                Console.WriteLine($"Loan Application No. {loanApplication.Id} Failed, Reason: {loanApplication.RejectionReason}");
+            }
+            Console.ReadLine();
+        }
+
+        public static void ValidateLoan(LoanApplication loanApplication) 
+        {
+
+            if (loanApplication.LoanAmount > 1500000 || loanApplication.LoanAmount < 100000)
+            {
+                loanApplication.LoanApproved = false;
+                loanApplication.RejectionReason = "Outside Of Scope";
+            }
+            else if (
+                        (loanApplication.LoanAmount > 1000000) &&
+                        ( (loanApplication.LTV() > 60) ||
+                            (loanApplication.CreditScore <= 950))
+                    )
+                {
+                    loanApplication.LoanApproved = false;
+                    loanApplication.RejectionReason = "Invalid Loan Amount - LTV - Credit Score amounts";
+                }
+            else if (loanApplication.LoanAmount < 1000000)
+                {
+                    loanApplication.LoanApproved = true;
+                    loanApplication.RejectionReason = "TODO: RAN OUT OF TIME";
+                }
+            else
+            {
+                loanApplication.LoanApproved = true;
+            }
+        }
+    }
+}
